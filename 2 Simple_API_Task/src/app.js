71afd23be602d9035cb09ebd4ed530d87cbbc239
Alekhya Ghosh <officialalekhya2004@gmail.com>
2026-08-01 14:08:17 +0530

    Added Simple API Task

---
+++ 2 Simple_API_Task/src/app.js
@@ -0,0 +1,50 @@
+const express=require('express')
+
+const app=express()
+app.use(express.json())
+
+const notes=[]
+// post
+app.post('/notes',(req,res)=>{
+    // console.log(req.body);
+    notes.push(req.body)
+    
+    res.status(201).json({
+        message:"Notes created Successfully"
+    })
+})
+// get
+app.get('/notes',(req,res)=>{
+
+
+    res.status(200).json({
+        message:"Notes Fetched Successfully",
+        notes:notes
+    })
+})
+
+
+// Delete
+app.delete('/notes/:index',(req,res)=>{
+    const index=req.params.index;
+
+    delete notes[index];
+
+    res.status(200).json({
+        message:"Note deleted Successfully"
+    })
+})
+
+// Patch
+app.patch('/notes/:index',(req,res)=>{
+    const index=req.params.index;
+    const description=req.body.description
+
+    notes[index].description=description
+
+    res.status(200).json({
+        message:"Note Updated Successfully"
+    })
+})
+
+module.exports=app
