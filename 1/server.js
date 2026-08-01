ecbe9919b6c4071c0980c2080585c33ac74c6459
Alekhya200408 <officialalekhya2004@gmail.com>
2026-08-01 10:34:02 +0530

    first commit

---
+++ 1/server.js
@@ -0,0 +1,17 @@
+const express=require('express')
+
+const app=express()
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('About Page')
+})
+app.get('/contact',(req,res)=>{
+    res.send('About Page')
+})
+
+app.listen(3000)
